bfef76def08fa9ddc6c8872b5b536ffbfbe63248
Angela Gloyna <angela.gloyna@gmail.com>
2020-05-04 15:23:53 -0500

    play around with api

--- constructor/fcp.py
+++ constructor/fcp.py
@@ -187,9 +187,9 @@ def _main(name, version, download_dir, platform, channel_urls=(), channels_remap
     precs = list(solver.solve_final_state())
     for prec in precs:
         all_packages = SubdirData.query_all(prec.name, channels=channel_urls, subdirs=[platform])
-        print(all_packages)
-        #most_recent = sorted(all_packages, key="version", reverse=True)
-        #print(most_recent)
+        #print(all_packages)
+        most_recent = sorted(all_packages, key="name")
+        print(most_recent)
         print()
         print()
         print('did that work at all?')
