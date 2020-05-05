37c6c031a5cb631fa4836ef40164430f424db875
Angela Gloyna <angela.gloyna@gmail.com>
2020-05-04 22:14:55 -0500

    clean up

--- constructor/fcp.py
+++ constructor/fcp.py
@@ -54,7 +54,7 @@ def _find_out_of_date_precs(precs, channel_urls, platform):
     out_of_date_package_records = {}
     for prec in precs:
         all_packages = SubdirData.query_all(prec.name, channels=channel_urls, subdirs=[platform])
-        all_packages = set()
+        all_packages = list()
         most_recent = max(all_packages, key=lambda package_version: (parse_version(package_version.version), package_version.build_number))
         prec_version = parse_version(prec.version)
         latest_version = parse_version(most_recent.version)
